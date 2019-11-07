f819e30969656499b3030986e2e2ed4506fc5e79
juan <mtzn_juandaniel@yahoo.com.mx>
2019-11-06 23:43:45 -0700

    Quiz view

---
+++ quiz.php
@@ -0,0 +1,9 @@
+<?php
+require 'private/config.php';
+require 'php/database.php';
+
+$stylesheet = 'quiz';
+
+require 'views/head.view.php';
+require 'views/quiz.view.php';
+?>
---
+++ views/quiz.view.php
@@ -0,0 +1,59 @@
+<body>
+    <header>
+        <img src="img/FULL-CUERVO-WHITE-NOBG.png" alt="Cuervo Nutrition" class="logo">
+
+        <a href="https://www.instagram.com/cuervo.mx/" target="_blank"><i class="fab fa-instagram"></i></a>
+    </header>
+
+    <div class="wrapped-content">
+        <div class="container c01">
+            <div class="titleCont">
+                <h2>Datos</h2>
+
+                <h2>Personales</h2>
+            </div>
+            
+            <button class="roundBtn btn01" id="btn01">Completar</button>
+        </div>
+
+        <div class="container c02">
+            <div class="titleCont">
+                <h2>Antecedentes</h2>
+
+                <h2>de</h2>
+
+                <h2>Salud</h2>
+            </div>
+            
+            <button class="roundBtn btn02" id="btn02">Completar</button>
+        </div>
+
+        <div class="container c03">
+            <div class="titleCont">
+                <h2>Antecedentes</h2>
+
+                <h2>Familiares</h2>
+            </div>
+            
+            <button class="roundBtn btn03" id="btn03">Completar</button>
+        </div>
+
+        <div class="container c04">
+            <div class="titleCont">
+                <h2>Alimentos</h2>
+
+                <h2>Y</h2>
+
+                <h2>Deportes</h2>
+            </div>
+            
+            <button class="roundBtn btn04" id="btn04">Completar</button>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy 2019 <a href="https://www.juandamartinez.com/" target="_blank">Juan Daniel Martínez</a></p>
+    </footer>
+</body>
+
+</html>
