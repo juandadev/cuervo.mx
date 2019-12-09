7d86f357b84a831acd6440b40176f6b741d9708c
juan <mtzn_juandaniel@yahoo.com.mx>
2019-12-09 16:13:27 -0700

    Fixed: Don't show fields without name

--- php/readData.php
+++ php/readData.php
@@ -13,15 +13,17 @@ if ($con) {
     $response = [];
     
     for ($i = 0; $i < count($clients); $i++) {
-        $clients2 = [
-            'id' => $clients[$i]['id_client'],
-            'name' => $clients[$i]['name_client'],
-            'age' => $clients[$i]['age_client'],
-            'gender' => $clients[$i]['gender_client'],
-            'phone' => $clients[$i]['phone_client'],
-            'date' => str_replace('-', '/', substr($clients[$i]['date_quiz'], 0, 10))
-        ];
-        array_push($response, $clients2);
+        if ($clients[$i]['name_client'] != "") {
+            $clients2 = [
+                'id' => $clients[$i]['id_client'],
+                'name' => $clients[$i]['name_client'],
+                'age' => $clients[$i]['age_client'],
+                'gender' => $clients[$i]['gender_client'],
+                'phone' => $clients[$i]['phone_client'],
+                'date' => str_replace('-', '/', substr($clients[$i]['date_quiz'], 0, 10))
+            ];
+            array_push($response, $clients2);
+        }
     }
 } else {
     $response = [
