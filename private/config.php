d8d40fea3e85e106fe1bd32594141db671729b55
juan <mtzn_juandaniel@yahoo.com.mx>
2020-02-11 17:03:13 -0700

    .

---
+++ private/config.php
@@ -0,0 +1,10 @@
+<?php
+$db_config = array(
+    'host' => 'localhost',
+    'database' => 'cuervo_nut',
+    'user' => 'juan',
+    'pass' => 'juan15410596'
+);
+
+$url = 'http://localhost/cuervo-nut';
+?>
